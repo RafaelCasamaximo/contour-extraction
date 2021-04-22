b73bd777c580d9e86d727077eb68ba8657b516a5
Rafael Furlanetto <rafael.furlanetto@uel.br>
2021-04-21 21:12:38 -0300

    Implementando Abrir imagem na GUI

--- interface.py
+++ interface.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, filedialog
 
 class Interface:
     def __init__(self):
@@ -14,11 +14,21 @@ class Interface:
         tabs_notebook = ttk.Notebook(self.root)
         tabs_notebook.pack()
 
-        frame_raw_image = Frame(tabs_notebook, width=500, height=500, bg="blue")
-        frame_mask_image = Frame(tabs_notebook, width=500, height=500, bg="red")
+        frame_mask_image = Frame(tabs_notebook, width=500, height=500)
+        frame_contour_image = Frame(tabs_notebook, width=500, height=500)
+        frame_sections_image = Frame(tabs_notebook, width=500, height=500)
 
-        frame_raw_image.pack(fill="both", expand=1)
         frame_mask_image.pack(fill="both", expand=1)
+        frame_contour_image.pack(fill="both", expand=1)
+        frame_sections_image.pack(fill="both", expand=1)
 
-        tabs_notebook.add(frame_raw_image, text='Tab 1')
-        tabs_notebook.add(frame_mask_image, text='lalla')
+        abre_arquivo_fotografia_botao = Button(frame_mask_image, text='Inserir', command=self.abre_arquivo_fotografia) 
+        abre_arquivo_fotografia_botao.grid(row=0, column=0, padx=10, pady=10)
+
+        tabs_notebook.add(frame_mask_image, text='Máscara')
+        tabs_notebook.add(frame_contour_image, text='Contorno')
+        tabs_notebook.add(frame_sections_image, text='Seções')
+
+    def abre_arquivo_fotografia(self):
+        self.root.filename = filedialog.askopenfilename(initialdir='./', title='Escolha um arquivo de imagem', filetype=(('Png Files', '*.png'),("All Files", "*.*")))
+        print(self.root.filename)
